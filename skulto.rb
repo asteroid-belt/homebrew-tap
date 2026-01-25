d87590b784b0c6cb0393e0e1a7caaa88427836db
Adam Cobb <adamos486@gmail.com>
2026-01-24 16:58:53 -0800

    v1.0.3 promotion

--- skulto.rb
+++ skulto.rb
@@ -1,25 +1,25 @@
 class Skulto < Formula
   desc "Offline-first tool for syncing and managing agent skills"
   homepage "https://github.com/asteroid-belt/skulto"
-  version "1.0.2"
+  version "1.0.3"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/asteroid-belt/skulto/releases/download/v1.0.2/skulto-v1.0.2-darwin-amd64.tar.gz"
-      sha256 "938340a3101b22ba3d80f6ad4aefa964931af47fb13a10043dc3ad6643199e4d"
+      url "https://github.com/asteroid-belt/skulto/releases/download/v1.0.3/skulto-v1.0.3-darwin-amd64.tar.gz"
+      sha256 "a76f803fd950a70fb8982a56ab2cbf8d0624ede55cdba1d1bf926416f0317eff"
     elsif Hardware::CPU.arm?
-      url "https://github.com/asteroid-belt/skulto/releases/download/v1.0.2/skulto-v1.0.2-darwin-arm64.tar.gz"
-      sha256 "67c5b636768245c23fcdec3d432276db68fd0d5330de80afa2b3e2da1aed6b26"
+      url "https://github.com/asteroid-belt/skulto/releases/download/v1.0.3/skulto-v1.0.3-darwin-arm64.tar.gz"
+      sha256 "3e9c6c6fc8d4cf31b9474dfec6e4f724a3c6ea0dd9d0152663f11fc9750e92c9"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/asteroid-belt/skulto/releases/download/v1.0.2/skulto-v1.0.2-linux-amd64.tar.gz"
-      sha256 "d2c0332d2ecf8c4c876a73a49a77c6291a6d48f5061a090c7fea0843abf6afea"
+      url "https://github.com/asteroid-belt/skulto/releases/download/v1.0.3/skulto-v1.0.3-linux-amd64.tar.gz"
+      sha256 "ccd8d60e590b83f6b2518a9cdfab04c8f6761f985fd011b06ee426f45f14458c"
     elsif Hardware::CPU.arm?
-      url "https://github.com/asteroid-belt/skulto/releases/download/v1.0.2/skulto-v1.0.2-linux-arm64.tar.gz"
-      sha256 "b461d73512ef80415b8049d93bd2e5341ae472779f2ab2331fe5fb2fb1113cdf"
+      url "https://github.com/asteroid-belt/skulto/releases/download/v1.0.3/skulto-v1.0.3-linux-arm64.tar.gz"
+      sha256 "f3ffcf7c8e69210e9d32eae074ac78e81eec07533758249009c2bd553e27c109"
     end
   end
 
